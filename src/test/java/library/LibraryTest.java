908d269065896f68cdb3f2bbd46c140baefe4e93
Israt Ahmed <israt.jahan237@gmail.com>
2022-08-24 10:10:12 +0100

    modified LibraryTest

--- src/test/java/library/LibraryTest.java
+++ src/test/java/library/LibraryTest.java
@@ -10,26 +10,46 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LibraryTest {
 
-    private Library library;
-    private Enclosure enclosure1;
-    private Enclosure enclosure2;
-    private Enclosure enclosure3;
+    Library library;
+
+    Book book;
+
+    //private Library library;
+    //private Enclosure enclosure1;
+    //private Enclosure enclosure2;
+    //private Enclosure enclosure3;
+
+    //@BeforeEach
+    //public void setup(){
+    //    enclosure1 = new Enclosure("test enclosure 1");
+    //   enclosure2 = new Enclosure("test enclosure 2");
+    //    enclosure3 = new Enclosure("test enclosure 3");
+    //    library = new Library(enclosure1, enclosure2, enclosure3);
+    //}
 
     @BeforeEach
     public void setup(){
-        enclosure1 = new Enclosure("test enclosure 1");
-        enclosure2 = new Enclosure("test enclosure 2");
-        enclosure3 = new Enclosure("test enclosure 3");
-        library = new Library(enclosure1, enclosure2, enclosure3);
+        book = new Book("Brave New World", "Aldous Huxley", "Nightmarish vision of a future society", 11);
+        library = new Library();
     }
 
+    //@Test
+    //public void canAddBookToEnclosure(){
+    //    Publication publication = new Book("Brave New World", "Aldous Huxley", "Nightmarish vision of a future society", 11);
+    //    library.addPublicationtoEnclosure(publication, enclosure1);
+    //    int expected = 1;
+    //    int actual = enclosure1.countPublications();
+    //    assertEquals(expected, actual);
+    //}
+
     @Test
-    public void canAddAnimalToEnclosure(){
-        Publication publication = new Book("Brave New World", "Aldous Huxley", "Nightmarish vision of a future society", 11);
-        library.addPublicationtoEnclosure(publication, enclosure1);
+    public void canAddBookToLibrary(){
+        library.addBook(book);
         int expected = 1;
-        int actual = enclosure1.countPublications();
+        int actual = library.getBooks().size();
+        //assertThat(expected).isEqualTo(actual);
         assertEquals(expected, actual);
+        System.out.println(library.getBooks());
     }
 
 }
