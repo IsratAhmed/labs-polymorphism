d60e9d506134eb28b596c2c0f0fad43a54b63cd7
Israt Ahmed <israt.jahan237@gmail.com>
2022-08-24 09:57:33 +0100

    modified Library

--- src/main/java/library/Library.java
+++ src/main/java/library/Library.java
@@ -1,43 +1,64 @@
 package library;
 
+import interfaces.IBook;
+import interfaces.IMagazine;
+import interfaces.INewspaper;
 import publication.Publication;
 import enclosures.Enclosure;
 
+import java.util.ArrayList;
+
 public class Library {
 
-    private Enclosure book;
-    private Enclosure magazine;
-    private Enclosure newspaper;
+    //private Enclosure book;
 
-    public Library(Enclosure book, Enclosure magazine, Enclosure newspaper) {
-        this.book = book;
-        this.magazine = magazine;
-        this.newspaper = newspaper;
+    private ArrayList<IBook> book;
+    //private Enclosure magazine;
+    private ArrayList<IMagazine> magazine;
+    //private Enclosure newspaper;
+    private ArrayList<INewspaper> newspaper;
+
+    public Library() {
+        this.book = new ArrayList<>();
+        this.magazine = new ArrayList<>();
+        this.newspaper = new ArrayList<>();
     }
 
-    public Enclosure getBook() {
-        return book;
+    public void addBook(IBook book){
+        this.book.add(book);
     }
 
-    public void setBook(Enclosure book) {
-        this.book = book;
+    public ArrayList<IBook> getBooks() {
+        return this.book;
     }
 
-    public Enclosure getMagazine() {
-        return magazine;
+    //public void setBook(Enclosure book) {
+    //    this.book = book;
+    //}
+
+    public void addMagazine(IMagazine magazine){
+        this.magazine.add(magazine);
     }
 
-    public void setMagazine(Enclosure magazine) {
-        this.magazine = magazine;
+    public ArrayList<IMagazine> getMagazines() {
+        return this.magazine;
     }
 
-    public Enclosure getNewspaper() {
-        return newspaper;
+    //public void setMagazine(Enclosure magazine) {
+    //    this.magazine = magazine;
+    //}
+
+    public void addNewspaper(INewspaper newspaper){
+        this.newspaper.add(newspaper);
     }
 
-    public void setNewspaper(Enclosure newspaper) {
-        this.newspaper = newspaper;
+    public ArrayList<INewspaper> getNewspapers() {
+        return this.newspaper;
     }
+
+    //public void setNewspaper(Enclosure newspaper) {
+    //    this.newspaper = newspaper;
+    //}
 
     public void addPublicationtoEnclosure(Publication publication, Enclosure enclosure) {
         enclosure.addPublication(publication);
